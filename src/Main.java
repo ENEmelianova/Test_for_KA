6335645fc9dfc2d4ca4239908d1e48ebccad7ce0
ENEmelianova <127324550+ENEmelianova@users.noreply.github.com>
2023-03-09 17:05:33 +0300

    exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("input:");
-        String str;
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("input:");
+            String str;
 
-        str = sc.nextLine();
+            str = sc.nextLine();
 
-        String result = MyCalc.myCalc(str);
+            String result = MyCalc.myCalc(str);
 
-        System.out.println("output:");
-        System.out.println(result);
+            System.out.println("output:");
+            System.out.println(result);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/MyCalc.java
+++ src/MyCalc.java
@@ -1,5 +1,7 @@
+import java.io.IOException;
+
 public class MyCalc {
-    public static int rom_to_int(String rom_num) {
+    public static int rom_to_int(String rom_num) throws IOException {
         String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
 
         int len = rom_num.length();
@@ -13,8 +15,7 @@ public class MyCalc {
             } else if (rom_num.equals(roman[10])) {
                 return 10;
             } else {
-                System.out.println("Error: wrong input");
-                return -1;
+                throw new IOException("Error: wrong input1");
             }
         } else if (len == 2) {
             if (rom_num.equals(roman[2])) {
@@ -26,49 +27,56 @@ public class MyCalc {
             } else if (rom_num.equals(roman[9])) {
                 return 9;
             } else {
-                System.out.println("Error: wrong input");
-                return -1;
+                throw new IOException("Error: wrong input2");
             }
         } else if (len == 3) {
             if (rom_num.equals(roman[3])) {
                 return 3;
             } else if (rom_num.equals(roman[7])) {
                 return 7;
-            } else if (rom_num.equals(roman[8])) {
+            } else {
+                throw new IOException("Error: wrong input3");
+            }
+        } else if (len == 4) {
+            if (rom_num.equals(roman[8])) {
                 return 8;
             } else {
-                System.out.println("Error: wrong input");
-                return -1;
+                throw new IOException("Error: wrong input4");
             }
         } else {
-            System.out.println("Error: wrong input (too long first param");
-            return -1; // если число длиннее 3-х символов, то ошибка
+            throw new IOException("Error: wrong input (too long)");
         }
     }
-    public static String myCalc(String str) {
+    public static String myCalc(String str) throws IOException {
+        if (str.length() < 2) {
+            throw new IOException("Error: wrong input");
+        }
+
         char[] arabic = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
-        int a = -1;
+        int first_number = -1;
         int position_operation = 1;
 
-        AR_ROM ar_rom = AR_ROM.NOT_SET;
+        AR_ROM first_number_type = AR_ROM.NOT_SET;
 
-        if (str.charAt(0) == arabic[0]) { a = 0; ar_rom = AR_ROM.AR; } else
+        if (str.charAt(0) == arabic[0]) { first_number = 0; first_number_type = AR_ROM.AR; } else
         if (str.charAt(0) == arabic[1]) {
-            a = 1; ar_rom = AR_ROM.AR;
-            if (str.charAt(1) == '0') {
-                a = 10;
+            first_number = 1; first_number_type = AR_ROM.AR;
+            if (str.charAt(1) == arabic[0]) {
+                first_number = 10;
                 position_operation = 2;
             }
         } else
-        if (str.charAt(0) == arabic[2]) { a = 2; ar_rom = AR_ROM.AR; } else
-        if (str.charAt(0) == arabic[3]) { a = 3; ar_rom = AR_ROM.AR; } else
-        if (str.charAt(0) == arabic[4]) { a = 4; ar_rom = AR_ROM.AR; } else
-        if (str.charAt(0) == arabic[5]) { a = 5; ar_rom = AR_ROM.AR; } else
-        if (str.charAt(0) == arabic[6]) { a = 6; ar_rom = AR_ROM.AR; } else
-        if (str.charAt(0) == arabic[7]) { a = 7; ar_rom = AR_ROM.AR; } else
-        if (str.charAt(0) == arabic[8]) { a = 8; ar_rom = AR_ROM.AR; } else
-        if (str.charAt(0) == arabic[9]) { a = 9; ar_rom = AR_ROM.AR; } else {
+        if (str.charAt(0) == arabic[2]) { first_number = 2; first_number_type = AR_ROM.AR; } else
+        if (str.charAt(0) == arabic[3]) { first_number = 3; first_number_type = AR_ROM.AR; } else
+        if (str.charAt(0) == arabic[4]) { first_number = 4; first_number_type = AR_ROM.AR; } else
+        if (str.charAt(0) == arabic[5]) { first_number = 5; first_number_type = AR_ROM.AR; } else
+        if (str.charAt(0) == arabic[6]) { first_number = 6; first_number_type = AR_ROM.AR; } else
+        if (str.charAt(0) == arabic[7]) { first_number = 7; first_number_type = AR_ROM.AR; } else
+        if (str.charAt(0) == arabic[8]) { first_number = 8; first_number_type = AR_ROM.AR; } else
+        if (str.charAt(0) == arabic[9]) { first_number = 9; first_number_type = AR_ROM.AR; } else {
+            // сюда попадем, если первый символ -- не арабская цифра
+
             // ищем знак (начинаем со второго по счёту символа)
             position_operation = -1;
             int i = 1;
@@ -80,83 +88,87 @@ public class MyCalc {
                     i = i + 1;
                 }
             }
-            if (position_operation == - 1) {
-                System.out.println("Error: wrong input (no operator)");
-                a = -1;
+            if (position_operation == -1) {
+                throw new IOException("Error: wrong input (no operation)");
             } // не нашли знак
 
             // переводим число до знака в int
             String rom_num = str.substring(0, position_operation);
-            a = rom_to_int(rom_num);
-
-            if (a != -1) {
-                ar_rom = AR_ROM.ROM; // если число было римское хорошее, устанавливаем флаг
-            }
+            first_number = rom_to_int(rom_num);
+            first_number_type = AR_ROM.ROM; // если число было римское хорошее, устанавливаем флаг
         }
 
         char operator = str.charAt(position_operation);
 
-        if (ar_rom == AR_ROM.NOT_SET) {
-            System.out.println("Error: wrong input");
-            return ""; // from myCalc
-        }
+        if (first_number_type == AR_ROM.NOT_SET) {
+            throw new IOException("Error: wrong input (invalid first number)");
+        } // первый символ не подходит
 
         // теперь работаем с b
-        int b = -1;
-        int bp = position_operation + 1;
+        int second_number = -1;
+        int second_number_position = position_operation + 1;
+        AR_ROM second_number_type = AR_ROM.NOT_SET;
 
-        if (str.charAt(bp) == arabic[0]) { b = 0;} else
-        if (str.charAt(bp) == arabic[1]) {
-            b = 1;
-            if ((str.length() > (bp + 1)) && (str.charAt(bp + 1) == '0')) {
-                b = 10;
+        if (str.charAt(second_number_position) == arabic[0]) { second_number = 0; second_number_type = AR_ROM.AR;} else
+        if (str.charAt(second_number_position) == arabic[1]) {
+            second_number = 1; second_number_type = AR_ROM.AR;
+            if ((str.length() > (second_number_position + 1)) && (str.charAt(second_number_position + 1) == '0')) {
+                second_number = 10;
             }
         } else
-        if (str.charAt(bp) == arabic[2]) { b = 2;} else
-        if (str.charAt(bp) == arabic[3]) { b = 3;} else
-        if (str.charAt(bp) == arabic[4]) { b = 4;} else
-        if (str.charAt(bp) == arabic[5]) { b = 5;} else
-        if (str.charAt(bp) == arabic[6]) { b = 6;} else
-        if (str.charAt(bp) == arabic[7]) { b = 7;} else
-        if (str.charAt(bp) == arabic[8]) { b = 8;} else
-        if (str.charAt(bp) == arabic[9]) { b = 9;} else {
-            if (ar_rom == AR_ROM.AR) {
-                System.out.println("Error: wrong input (first arg is arabic)");
-                return "";
-            } else { // если первое было норм римским
-                String rom_num = str.substring(bp);
-                b = rom_to_int(rom_num);
+        if (str.charAt(second_number_position) == arabic[2]) { second_number = 2; second_number_type = AR_ROM.AR;} else
+        if (str.charAt(second_number_position) == arabic[3]) { second_number = 3; second_number_type = AR_ROM.AR;} else
+        if (str.charAt(second_number_position) == arabic[4]) { second_number = 4; second_number_type = AR_ROM.AR;} else
+        if (str.charAt(second_number_position) == arabic[5]) { second_number = 5; second_number_type = AR_ROM.AR;} else
+        if (str.charAt(second_number_position) == arabic[6]) { second_number = 6; second_number_type = AR_ROM.AR;} else
+        if (str.charAt(second_number_position) == arabic[7]) { second_number = 7; second_number_type = AR_ROM.AR;} else
+        if (str.charAt(second_number_position) == arabic[8]) { second_number = 8; second_number_type = AR_ROM.AR;} else
+        if (str.charAt(second_number_position) == arabic[9]) { second_number = 9; second_number_type = AR_ROM.AR;} else {
+            if (first_number_type == AR_ROM.AR) {
+                throw new IOException("Error: wrong input (ar-rom mismatch)"); // если первое было арабским, а второе римским
+            } else {
+                String rom_num = str.substring(second_number_position);
+                second_number = rom_to_int(rom_num);
+                second_number_type = AR_ROM.ROM;
             }
         }
-        if (b < 0) {
-            System.out.println("Error: wrong input (invalid second arg)");
-            return "";
+
+        if (second_number < 0) {
+            throw new IOException("Error: wrong input (invalid second number)");
         }
 
+        if (first_number_type != second_number_type) {
+            throw new IOException("Error: wrong input (ar-rom mismatch)");
+        }
 
         int result = 0;
         if (operator == '+') {
-            result = a + b;
-        }
-        if (operator == '-') {
-            result = a - b;
-        }
-        if (operator == '*') {
-            result = a * b;
-        }
-        if (operator == '/') {
-            if (b == 0) {
-                System.out.println("Error: divide on zero");
-                return "";
+            result = first_number + second_number;
+        } else if (operator == '-') {
+            result = first_number - second_number;
+            if (first_number_type == AR_ROM.ROM && result < 0) { // отрицаельная разность для римских
+                throw new IOException("Error: wrong input (negative roman result)");
             }
-            result = a / b;
+        } else if (operator == '*') {
+            result = first_number * second_number;
+        } else if (operator == '/') {
+            // Если второе число было нулем - поймаем
+            result = first_number / second_number;
+        } else {
+            throw new IOException("Error: wrong input (expected operation)"); // неподходящее первое число
         }
 
-        if (ar_rom == AR_ROM.AR) {
+        if (first_number_type == AR_ROM.AR) {
             return String.valueOf(result);
         } else {
-            String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX"};
+            String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                    "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+                    "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+                    "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+                    "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+                    "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
             return roman[result];
         }
     }
 }
+
